7edb4bed2ced9e8e69c9b31fc3f0f88b301b6a47
leticialbuk <leticia.albuk@gmail.com>
2023-03-21 17:01:16 -0300

    iniciando

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var price = 10.2;
+            //var texto = "O preço do produto é " + price + " apenas na promoção";
+            //var texto = string.Format("O preço do produto é {0} apenas na promoção", price);
+            var texto = $"O preço do produto é {price} apenas na promoção";
+
+            Console.WriteLine(texto);
+        }
+    }
+}
