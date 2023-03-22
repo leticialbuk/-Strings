f4c17673e3a03a314702b60b165e7dd1b3e6a644
leticialbuk <leticia.albuk@gmail.com>
2023-03-22 15:01:13 -0300

    comparacao com StartsWith e EndsWith

--- ComparacaoString/Program.cs
+++ ComparacaoString/Program.cs
@@ -5,11 +5,18 @@
         static void Main(string[] args)
         {
             var texto = "Este texto é um teste";
-            //Console.WriteLine(texto.CompareTo("Testando"));
-            //Console.WriteLine(texto.CompareTo("testando"));
+            Console.WriteLine(texto.CompareTo("Testando"));
+            Console.WriteLine(texto.CompareTo("testando"));
+
             Console.WriteLine(texto.Contains("teste"));
             Console.WriteLine(texto.Contains("Teste"));
             Console.WriteLine(texto.Contains("Teste", StringComparison.OrdinalIgnoreCase));
+
+            Console.WriteLine(texto.StartsWith("Este"));
+            Console.WriteLine(texto.StartsWith("este"));
+
+            Console.WriteLine(texto.EndsWith("teste"));
+            Console.WriteLine(texto.EndsWith("Teste"));
         }
     }
 }
