a2ce65e8d67971364227ddd60cdaadc6de7ef66f
leticialbuk <leticia.albuk@gmail.com>
2023-03-22 15:24:18 -0300

    Indices com IndexOf e LastIndexOf

---
+++ Indices/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var texto = "Este texto é um teste";
+            Console.WriteLine(texto.IndexOf("é"));
+            Console.WriteLine(texto.LastIndexOf("s"));
+        }
+    }
+}
