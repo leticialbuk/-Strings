d7df85c60b268c15870331455e64c3c2723a0dae
leticialbuk <leticia.albuk@gmail.com>
2023-03-23 15:08:09 -0300

    utilizando SringBuilder no texto

---
+++ StringBuilder/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var texto = new StringBuilder();
+            texto.Append("Este texto é um teste");
+            texto.Append("Este texto");
+            texto.Append("um teste");
+            texto.Append("Este é um teste");
+
+            Console.WriteLine(texto);
+        }
+    }
+}
