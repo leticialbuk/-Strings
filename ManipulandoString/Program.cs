b88b6948963c88ce0fd35580854d1f32d28df878
leticialbuk <leticia.albuk@gmail.com>
2023-03-23 14:46:31 -0300

    modificando string com Replace, Split e Trim

---
+++ ManipulandoString/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var texto = " Este texto é um teste ";
+            Console.WriteLine(texto.Replace("Este", "Isto"));
+
+            var divisao = texto.Split(" ");
+            Console.WriteLine(divisao[0]);
+            Console.WriteLine(divisao[1]);
+            Console.WriteLine(divisao[2]);
+            Console.WriteLine(divisao[3]);
+
+            var resultado = texto.Substring(5, 5);
+            Console.WriteLine(resultado);
+
+            Console.WriteLine(texto.Trim());
+        }
+    }
+}
