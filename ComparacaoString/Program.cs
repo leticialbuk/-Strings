b3f0a276d323f0e1827c3cebf75de0480df3c309
leticialbuk <leticia.albuk@gmail.com>
2023-03-23 15:20:13 -0300

    atualizando pastas

--- ComparacaoString/Program.cs
+++ ComparacaoString/Program.cs
@@ -1,12 +1,14 @@
-﻿namespace MyApp // Note: actual namespace depends on the project name.
+﻿using System;
+
+namespace MyApp
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             var texto = "Este texto é um teste";
-            Console.WriteLine(texto.CompareTo("Testando"));
-            Console.WriteLine(texto.CompareTo("testando"));
+            Console.WriteLine(texto.CompareTo("Teste"));
+            Console.WriteLine(texto.CompareTo("teste"));
 
             Console.WriteLine(texto.Contains("teste"));
             Console.WriteLine(texto.Contains("Teste"));
