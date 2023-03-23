acb5fbf60ce95591f0821d8f526e1e4c2486c774
leticialbuk <leticia.albuk@gmail.com>
2023-03-23 14:17:04 -0300

    modificando texto M/m com ToLower e ToUpper

---
+++ MetodosAdicionais/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var texto = "Este texto é um teste";
+            Console.WriteLine(texto.ToLower());
+            Console.WriteLine(texto.ToUpper());
+        }
+    }
+}
